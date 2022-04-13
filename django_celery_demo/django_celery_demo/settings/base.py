5b260aea18a793d8ad18d89d6fceec8d0913085c
Driti Tannk <driti.tannk@thegatewaycorp.com>
2022-04-13 17:53:45 +0530

    Add Celery Task For Generating Random Users
    	Install And Configure Celery,
    	Add Tasks File For Generating Users,
    	Update Requirements File.

--- django_celery_demo/django_celery_demo/settings/base.py
+++ django_celery_demo/django_celery_demo/settings/base.py
@@ -100,3 +100,7 @@ MEDIA_URL = "/media/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# celery
+CELERY_BROKER_URL = 'amqp://localhost'
+
